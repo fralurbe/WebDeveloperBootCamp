ffd4fc75e8e4347ad4e97d70ea88d41dd04ce740
Fran Lurbe <francisco.lurbe@gmail.com>
2023-03-15 11:51:43 +0100

    Inicio del proyecto, arrancar con nodemon app.js

---
+++ YelpCamp/app.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+const express = require ('express');
+const app = express();
+const path = require('path');
+
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.listen(3000, () => {
+    console.log('Serving on port 3000');
+})
